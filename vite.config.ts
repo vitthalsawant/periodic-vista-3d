dcb39fd16d9e185952ebaf6603a7fced0a5a1237
Vitthal Sawant <vitthalsawant300@gmail.com>
2025-05-18 19:44:20 +0530

    add element info

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
   plugins: [
     react(),
     mode === 'development' &&
